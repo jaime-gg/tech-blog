5379f70cbde883c074860b031a8a2e4024f37d9e
jester <jaimebuddy98@gmail.com>
2022-02-24 16:01:47 -0600

    added get route to homepage-routes

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,7 +1,41 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
-
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'body',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', {
+          posts,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 
 
