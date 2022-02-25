fc9991d62487de9376892d2e83a36bf6f6bb9ecb
jester <jaimebuddy98@gmail.com>
2022-02-24 18:37:57 -0600

    altered the edit route for posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/with-auth');
 
 router.get('/', (req, res) => {
     Post.findAll({
